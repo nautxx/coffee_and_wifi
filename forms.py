95c97f7c0662c2b087b03917aa2d96ebaa9c2bd2
naut <jrbarnachea@gmail.com>
2022-05-25 09:34:34 -0700

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm # pip install -U Flask-WTF
 from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired, URL
 
